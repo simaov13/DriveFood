963dda4e68336dff05e9031f9ec8e0f248807d65
LaraRibeiro0 <61421586+LaraRibeiro0@users.noreply.github.com>
2020-12-17 11:10:35 +0000

    editar /eliminar utilizador #78

--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -1,1 +1,37 @@
 const db = require('../config/sqlite');
+
+
+
+//editar utilizador
+exports.editarUtilizador = (req, res) => {
+    try {
+        //se ele for diferente user dá erro, se nao executa
+        if (req.body.type != 'user') return res.status(406).send({ message: 'Tipo de utilizador inválido ("user"/"driver"/"merchant")' });
+        //alterar utilizador
+        let sql = 'UPDATE user set username = ?, password = ?, adress = ?, postal_code = ?, city = ?, phone = ?, email =? WHERE  id_utilizador = ?'
+        db.get(sql, [username, password, adress, postal_code, city, phone, email], (err, result) => {
+            if (err) return res.status(500).send(err.message);
+            return res.json(result).send({ message: 'Restaurante editado com sucesso' });
+
+        });
+    } catch (err) {
+        return res.status(500).send({ message: err.message });
+    }
+};
+
+//eliminar utilizador
+exports.eliminarUtilizador = (req, res) => {
+    try {
+        //se ele for diferente user dá erro, se nao executa
+        if (req.body.type != 'user') return res.status(406).send({ message: 'Tipo de utilizador inválido ("user"/"driver"/"merchant")' });
+        //eliminar user
+        let sql = 'DELETE * FROM user WHERE id_utilizador = ?';
+        db.get(sql, [req.params.id_utilizador], (err, result) => {
+            if (err) return res.status(500).send(err.message);
+
+            return res.json(result);
+        });
+    } catch (err) {
+        return res.status(500).send({ message: err.message });
+    }
+};
--- Backend/routes/user-route.js
+++ Backend/routes/user-route.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/user-controller');
 
 //router.get('/users', userController.register);
-
+//editar utilizador
+//eliminar utilizador
+router.delete('/user/:id_utilizador', usernController.eliminarUtilizador);
 module.exports = router;
