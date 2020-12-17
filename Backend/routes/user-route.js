f671c5eaf114ca1e2786fff5b890600ad842930f
LaraRibeiro0 <61421586+LaraRibeiro0@users.noreply.github.com>
2020-12-17 11:12:01 +0000

    chmar funcao editar #78

--- Backend/routes/user-route.js
+++ Backend/routes/user-route.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/user-controller');
 
 //router.get('/users', userController.register);
 //editar utilizador
+router.put('user/:id_utilizador', userController.editarUtilizador);
 //eliminar utilizador
-router.delete('/user/:id_utilizador', usernController.eliminarUtilizador);
+router.delete('/user/:id_utilizador', userController.eliminarUtilizador);
 module.exports = router;
