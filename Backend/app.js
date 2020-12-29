0ce1118d7c1f4557348ed4ca717ec32d4551e013
LaraRibeiro0 <61421586+LaraRibeiro0@users.noreply.github.com>
2020-12-29 00:00:08 +0000

    porta

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const requireDir = require('require-dir');
 const bodyParser = require('body-parser');
 app.use(express.static('public'));
-const port = 3000;
+const port = 4000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
