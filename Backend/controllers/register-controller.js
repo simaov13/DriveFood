a43301ceb26cb23ef3bb71bf3c229041123b05f8
LaraRibeiro0 <61421586+LaraRibeiro0@users.noreply.github.com>
2021-01-08 23:47:19 +0000

    verificaçoes, adicionar cliente, empresa, condutor

--- Backend/controllers/register-controller.js
+++ Backend/controllers/register-controller.js
@@ -8,8 +8,8 @@ exports.register = (req, res) => {
         // req.body
         //user
         let username = req.body.username;
-        let name = req.body.name;
         let password = req.body.password;
+        let name = req.body.name;
         let nif = req.body.nif;
         let address = req.body.address;
         let postal_code = req.body.postal_code;
@@ -21,100 +21,70 @@ exports.register = (req, res) => {
         let type_license = req.body.type_license;
         let phone = req.body.phone;
         let phone_security = req.body.phone_security;
-        let security = req.body.security;
         //merchant
         let description = req.body.description;
         let logo = req.body.logo;
         let approved = 0;
 
-
-
-        // verificar se o username já existe
-        let sql = 'SELECT username FROM user WHERE username = ?';
-        db.get(sql, [username], (err, result) => {
-            if (err) res.status(500).send(err.message);
-            if (result) {
-                res.status(409).send({ message: 'Username já registado' });
-                //throw "error";
-            }
-        });
-
-        // verificar se email ja existe
-        let sql1 = 'SELECT email FROM user WHERE email = ?';
-        db.get(sql1, [email], (err, result) => {
-            if (err) {
-                res.status(500).send(err.message);
-            }
-            if (result) {
-                res.status(409).send({ message: 'Email já registado' });
-                //throw "error";
-            }
-        });
-
-        //verificar se telemovel ja existe 
-        let sql2 = 'SELECT phone FROM user WHERE phone = ?';
-        db.get(sql2, [phone], (err, result) => {
-            if (err) {
-                res.status(500).send(err.message);
-            }
-            if (result) {
-                res.status(409).send({ message: 'Telemóvel já registado' });
-                //throw "error";
-            }
-        });
-
-        //verificar se nif ja existe 
-        let sql3 = 'SELECT nif FROM user WHERE nif = ?';
-        db.get(sql3, [nif], (err, result) => {
-            if (err) {
-                res.status(500).send(err.message);
-            }
-            if (result) {
-                res.status(409).send({ message: 'Nif já registado' });
-                //throw "error";
-            }
-        });
-
         // Verificações
         //Username maior que 5 caracteres
-        if (username.length < 5) res.status(411).send({ message: 'O nome de utilizador tem de ter 5 ou mais caracteres' });
+        if (username.length < 5) {
+            res.status(411).send({ message: 'O nome de utilizador tem de ter 5 ou mais caracteres' });
+            throw "O nome de utilizador tem de ter 5 ou mais caracteres";
+        }
         //Palavra passe maior que 8 caracteres
-        if (password.length < 8) res.status(411).send({ message: 'A palavra-passe tem de ter 8 ou mais caracteres' });
+        if (password.length < 8) {
+            res.status(411).send({ message: 'A palavra-passe tem de ter 8 ou mais caracteres' });
+            throw 'A palavra-passe tem de ter 8 ou mais caracteres';
+        }
+        if (type == 'user' || type == 'merchant') {
+            //Não pode ser for diferente de 9
+            if (!Number.isInteger(nif) && nif.length != 9) {
+                res.status(406).send({ message: 'NIF inválido' });
+                throw "Nif inválido";
+            }
+        }
         //Não pode ser for diferente de 9
-        if (!Number.isInteger(nif) && nif.length != 9) res.status(406).send({ message: 'NIF inválido' });
-        //Não pode ser for diferente de 9
-        if (!Number.isInteger(phone) && nif.length != 9) res.status(406).send({ message: 'Telemovel inválido' });
+        if (!Number.isInteger(phone) && phone.length != 9) {
+            res.status(406).send({ message: 'Telemovel inválido' });
+            throw "Telemovel inválido";
+        }
         //Diferente de algum utilizador
-        if (type != 'user' && type != 'driver' && type != 'merchant') res.status(406).send({ message: 'Tipo de utilizador inválido ("user"/"driver"/"merchant")' });
+        if (type != 'user' && type != 'driver' && type != 'merchant') {
+            res.status(406).send({ message: 'Tipo de utilizador inválido ("user"/"driver"/"merchant"/"admin")' });
+            throw 'Tipo de utilizador inválido';
+        }
         //Se tipo for driver
-        if (type === 'driver') {
+        if (type == 'driver') {
             //Veiculo Proprio
             if (vehicle != 'sim' && vehicle != 'não') {
                 res.status(406).send({ message: 'Veiculo Próprio inválido ("sim" /"não")' });
+                throw "Veiculo Próprio inválido";
             }
             //Tipo de carta
             if (type_license != 'am' && type_license != 'a1' && type_license != 'a2' && type_license != 'b') {
-                res.status(406).send({ message: 'Tipo de veiculo inválido ("am" /"a1" /"a2" /"b" )' })
+                res.status(406).send({ message: 'Tipo de veiculo inválido ("am" /"a1" /"a2" /"b" )' });
+                throw "Tipo de veiculo inválido";
             }
             //Seguro de vida
-            if (security != 'sim' && security != 'não') {
-                res.status(406).send({ message: 'Seguro de vida inválido ("sim" /"não")' });
-            }
         }
         //hash
         let hash = bcrypt.hashSync(password, 10);
 
+
+
+
         //utilizador tipo driver
-        if (vehicle != null && type_license != null && phone != null && phone_security != null) {
+        if (vehicle != null && type_license != null && phone_security != null) {
             // Inserir um utilizador Driver
-            sql = 'INSERT INTO user (username, name, email, password, city, phone, phone_security, vehicle, type_license, type) VALUES (?,?,?,?,?,?,?,?,?,?)';
+            sql = 'INSERT INTO user (username,name, email, password, city, phone, phone_security, vehicle, type_license, type) VALUES (?,?,?,?,?,?,?,?,?,?)';
             db.run(sql, [username, name, email, hash, city, phone, phone_security, vehicle, type_license, type], (err) => {
                 if (err) {
-                    res.status(500).send(err.message);
+                    res.status(500).send(err.message)
                 } else {
                     res.status(201).send({
                         // Utilizador registado
-                        message: 'Entregador registado com sucesso',
+                        message: 'Utilizador registado com sucesso',
                         user: {
                             username: username,
                             name: name,
@@ -130,17 +100,18 @@ exports.register = (req, res) => {
                     });
                 }
             });
-        } else if (description != null || logo != null || phone !== null) {
+        } else if (description != null && logo != null && phone != null) {
             // Inserir um utilizador Merchant
-            sql = 'INSERT INTO user (name, email, password, address, city, phone, description, logo, type) VALUES (?,?,?,?,?,?,?,?,?)';
-            db.run(sql, [name, email, hash, address, city, phone, description, logo, type], (err) => {
+            sql = 'INSERT INTO user (username, name, email, password, address, city, phone, description, logo, type) VALUES (?,?,?,?,?,?,?,?,?,?)';
+            db.run(sql, [username, name, email, hash, address, city, phone, description, logo, type], (err) => {
                 if (err) {
                     res.status(500).send(err.message);
                 } else {
                     res.status(201).send({
                         // Utilizador registado
-                        message: 'Empresa registada com sucesso',
+                        message: 'Utilizador registado com sucesso',
                         user: {
+                            username: username,
                             name: name,
                             email: email,
                             password: hash,
@@ -155,12 +126,11 @@ exports.register = (req, res) => {
                 }
             });
         } else {
-
             // Inserir um utilizador User
             sql = 'INSERT INTO user (username, name, password, nif, address, postal_code , email, city, phone, type) VALUES (?,?,?,?,?,?,?,?,?,?)';
             db.run(sql, [username, name, hash, nif, address, postal_code, email, city, phone, type], (err) => {
                 if (err) {
-                    res.status(500).send(err.message);
+                    res.status(500).send(err.message)
                 } else {
                     res.status(201).send({
                         // Utilizador registado
@@ -183,8 +153,6 @@ exports.register = (req, res) => {
         }
     } catch (err) {
         console.log(err);
-        res.status(500).send({ message: err.message });
     }
-    db.close();
     return;
 };
