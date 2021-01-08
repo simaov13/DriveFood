7a171663347ab031a052e5d85f6e21e51de18f30
Simão Vilaça <simaov.13@gmail.com>
2021-01-08 14:30:04 +0000

    Delete Backend and Frontend folder

--- Backend/routes/login-route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const loginController = require('../controllers/login-controller');
-//login
-router.post('/login', loginController.login);
-
-module.exports = router;
--- Backend/routes/register-route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const registerController = require('../controllers/register-controller');
-
-//adicionar
-router.post('/register', registerController.register);
-
-module.exports = router;
