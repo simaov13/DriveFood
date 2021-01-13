6c2c7008ab9e669f923f413026ad7a3976594e8f
LaraRibeiro0 <61421586+LaraRibeiro0@users.noreply.github.com>
2021-01-13 01:38:47 +0000

    update criar e editar restaurantes

--- Backend/controllers/restaurant-controller.js
+++ Backend/controllers/restaurant-controller.js
@@ -48,9 +48,18 @@ exports.adicionarRestaurante = (req, res) => {
         let id_restaurante = req.body.id_restaurante;
         let name = req.body.name;
         let image = req.body.image;
-        let location = req.body.location;
+        let address = req.body.address;
         let phone = req.body.phone;
         let email = req.body.email;
+
+
+        //Diferente de um tipo de restaurante
+        if (type != 'Humburger' && type != 'Pizaria' && type != 'Chinês' && type != 'Japonês' && type != 'Rodizio') {
+            res.status(406).send({
+                message: 'Tipo de restaurante inválido ("Humburger"/"Pizaria"/"Chinês"/"Japones"/"Rodizio")'
+            });
+            throw 'Tipo de restaurante inválido';
+        }
 
         //se ele for diferente merchant dá erro, se nao executa
         if (req.body.type != 'merchant') res.status(406).send({ message: 'Tipo de utilizador inválido ("user"/"driver"/"merchant"/"admin")' });
@@ -71,20 +80,20 @@ exports.adicionarRestaurante = (req, res) => {
         if (req.body.type != 'merchant') res.status(406).send({ message: 'Tipo de utilizador inválido ("user"/"driver"/"merchant"/"admin")' });
 
         // criar restaurante
-        sql = 'INSERT INTO restaurante (id_restaurante, name, image, location, phone, email) VALUES (?,?,?,?,?,?)';
-        db.run(sql, [id_restaurante, name, image, location, phone, email], (err) => {
+        sql = 'INSERT INTO restaurante (name, image, address, phone, email,type_restaurante) VALUES (?,?,?,?,?,?)';
+        db.run(sql, [name, image, address, phone, email, type_restaurante], (err) => {
             if (err) {
                 res.status(500).send(err.message);
             } else {
                 res.status(201).send({
                     message: 'Restaurante adicionado com sucesso',
                     user: {
-                        id_restaurante: id_restaurante,
                         name: name,
                         image: image,
-                        location: location,
+                        address: address,
                         phone: phone,
                         email: email,
+                        type_restaurante
                     },
                 });
             }
@@ -100,15 +109,24 @@ exports.adicionarRestaurante = (req, res) => {
 //editar restaurante
 exports.editarRestaurante = (req, res) => {
     try {
+        // req.body
+        let id_restaurante = req.body.id_restaurante;
+        let name = req.body.name;
+        let image = req.body.image;
+        let address = req.body.address;
+        let phone = req.body.phone;
+        let email = req.body.email;
+
         //se ele for diferente merchant dá erro, se nao executa
         if (req.body.type != 'merchant') res.status(406).send({ message: 'Tipo de utilizador inválido ("user"/"driver"/"merchant"/"admin")' });
+
         //alterar restaurante
-        let sql = 'UPDATE restaurante set name = ?, image = ?, location = ?, phone = ?, email = ? WHERE  id_restaurante = ?'
-        db.get(sql, [name, image, location, phone, email], (err, result) => {
+        let sql = 'UPDATE restaurante set name = ?, image = ?, address = ?, phone = ?, email = ? WHERE  id_restaurante = ?'
+        db.get(sql, [name, image, address, phone, email, id_restaurante], (err) => {
             if (err) {
                 res.status(500).send(err.message);
             } else {
-                res.json(result).send({ message: 'Restaurante editado com sucesso' });
+                res.status(200).send({ message: 'Restaurante editado com sucesso' });
             }
         });
     } catch (err) {
@@ -133,9 +151,9 @@ exports.eliminarRestaurante = (req, res) => {
                 }
             }
         });
-        
+
         //eliminar restaurante
-        let sql1 = 'DELETE * FROM restaurante WHERE id = ?';
+        let sql1 = 'DELETE FROM restaurante WHERE id_restaurante = ?';
         db.get(sql, [req.params.id_restaurante], (err, result) => {
             if (err) {
                 res.status(500).send(err.message);
